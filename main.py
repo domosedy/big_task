d2358ec5914f709749832f596de8286274db710d
domosedy <52038168+domosedy@users.noreply.github.com>
2021-02-08 18:32:37 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+import requests
+
+
+def get_picture(url, spn, x, y, map_file):
+    params = {
+        'll': str(x) + ',' + str(y)
+        'spn': spn,
+        'l': 'sat',
+        'apikey': '40d1649f-0493-4b70-98ba-98533de7710b'
+    }
+
+    response = requests.get(url, params=params)
+    with open(map_file, 'w') as f:
+        f.write(response.content)
+
+
+def get_coords(name_of_object):
+    url = 'http://geocode-maps.yandex.ru/1.x/'
+    params = {
+        'apikey': '40d1649f-0493-4b70-98ba-98533de7710b',
+        'format': 'json',
+        'geocode': name_of_object
+    }
+
+    response = requests.get(url, params=params).json()
